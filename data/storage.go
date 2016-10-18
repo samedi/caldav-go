b16b799067f95c4c01323e7708cc783db06ab8a5
Daniel Ferraz <ferraz@samedi.de>
2016-10-18 14:30:47 +0200

    Add doc comments to the storage interface

--- data/storage.go
+++ data/storage.go
@@ -8,25 +8,43 @@ import (
   "git.samedi.cc/ferraz/caldav/files"
 )
 
-// The storage is the responsible for the CRUD operations on the caldav resources.
+// The Storage is the responsible for the CRUD operations on the caldav resources.
 type Storage interface {
+  // GetResources gets a list of resources based on a given `rpath`. The
+  // `rpath` is the path to the original resource that's being requested. The resultant list
+  // will/must contain that original resource in it, apart from any additional resources. It also receives
+  // `withChildren` flag to say if the result must also include all the original resource`s
+  // children (if original is a collection resource). If `true`, the result will have the requested resource + children.
+  // If `false`, it will have only the requested original resource (from the `rpath` path).
+  // It returns errors if anything went wrong or if it could not find any resource on `rpath` path.
   GetResources(rpath string, withChildren bool) ([]Resource, error)
-  GetResourcesByFilters(rpath string, filters *ResourceFilter) ([]Resource, error)
-  /* Fetch a list of resources by path from the storage
-  *
-  * This method should fetch all the `rpaths` and return an array
-  * of the reosurces found. No error 404 will be returned if an
-  * element cannot be found.
-  *
-  * errors can be returned if errors other than "not found" happened. */
+  // GetResourcesByList fetches a list of resources by path from the storage.
+  // This method fetches all the `rpaths` and return an array of the reosurces found.
+  // No error 404 will be returned if one of the resources cannot be found.
+  // Errors are returned if any errors other than "not found" happens.
   GetResourcesByList(rpaths []string) ([]Resource, error)
+  // GetResourcesByFilters returns the filtered children of a target collection resource.
+  // The target collection resource is the one pointed by the `rpath` parameter. All of its children
+  // will be checked against a set of `filters` and the matching ones are returned. The results
+  // contains only the filtered children and does NOT include the target resource. If the target resource
+  // is not a collection, an empty array is returned as the result.
+  GetResourcesByFilters(rpath string, filters *ResourceFilter) ([]Resource, error)
+  // GetResource gets the requested resource based on a given `rpath` path. It returns the resource (if found) or
+  // nil (if not found). Also returns a flag specifying if the resource was found or not.
   GetResource(rpath string) (*Resource, bool, error)
+  // IsResourcePresent checks if any resource exists on the given `rpath` path.
   IsResourcePresent(rpath string) bool
+  // CreateResource creates a new resource on the `rpath` path with a given `content`.
   CreateResource(rpath, content string) (*Resource, error)
+  // UpdateResource udpates a resource on the `rpath` path with a given `content`.
   UpdateResource(rpath, content string) (*Resource, error)
+  // DeleteResource deletes a resource on the `rpath` path.
   DeleteResource(rpath string) error
 }
 
+// FileStorage is the storage that deals with resources as files in the file system. So, a collection resource
+// is treated as a folder/directory and its children resources are the files it contains. On the other hand, non-collection
+// resources are just plain files.
 type FileStorage struct {
 }
 
@@ -79,9 +97,6 @@ func (fs *FileStorage) GetResourcesByFilters(rpath string, filters *ResourceFilt
   return result, nil
 }
 
-/*
- * Since file access is realtively cheap, we just read by fanning out to `GetResource`
- */
 func (fs *FileStorage) GetResourcesByList(rpaths []string) ([]Resource, error) {
   results := []Resource{}
 
