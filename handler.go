afdda6764bb38dcdba25de48b037248a8e4f264b
Daniel Ferraz <ferraz@samedi.de>
2016-10-19 11:13:49 +0200

    Restore the http.Request body after reading it, so that it can be read again of needed.

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package caldav
 
 import (
+  "bytes"
   "net/http"
   "io/ioutil"
   "git.samedi.cc/ferraz/caldav/handlers"
@@ -17,7 +18,13 @@ func HandleRequest(writer http.ResponseWriter, request *http.Request) *handlers.
   return handler.Handle()
 }
 
+// This function reads the request body and restore its content, so that
+// the request body can be read a second time.
 func readRequestBody(request *http.Request) string {
+  // Read the content
   body, _ := ioutil.ReadAll(request.Body)
+  // Restore the io.ReadCloser to its original state
+  request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+  // Use the content
   return string(body)
 }
